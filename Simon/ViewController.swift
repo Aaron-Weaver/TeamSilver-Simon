a97bac1e5214c8ef873615d3202c87e97dccdbff
Austin Keeton <akeeton94@gmail.com>
2015-09-10 12:15:27 -0500

    Change sequenceLight names. Add to list.

--- Simon/ViewController.swift
+++ Simon/ViewController.swift
@@ -10,12 +10,43 @@ import UIKit
 
 class ViewController: UIViewController, StateObserver {
     
+    /// Declare delegates for sequence lights
+    @IBOutlet weak var sequenceLight1: UIImageView!
+    @IBOutlet weak var sequenceLight2: UIImageView!
+    @IBOutlet weak var sequenceLight3: UIImageView!
+    @IBOutlet weak var sequenceLight4: UIImageView!
+    @IBOutlet weak var sequenceLight5: UIImageView!
+    @IBOutlet weak var sequenceLight6: UIImageView!
+    @IBOutlet weak var sequenceLight7: UIImageView!
+    @IBOutlet weak var sequenceLight8: UIImageView!
+    @IBOutlet weak var sequenceLight9: UIImageView!
+    @IBOutlet weak var sequenceLight10: UIImageView!
+    @IBOutlet weak var sequenceLight11: UIImageView!
+    @IBOutlet weak var sequenceLight12: UIImageView!
+    
+    
+    /// List containing the sequence of lights to indicate index in sequence
+    private var sequenceLightList = [(UIImageView)]()
+    
     /// Logic object for the Simon game.
     var gameLogic: GameLogic?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         gameLogic = GameLogic(stateObserver: self)
+        sequenceLightList.append(sequenceLight1)
+        sequenceLightList.append(sequenceLight2)
+        sequenceLightList.append(sequenceLight3)
+        sequenceLightList.append(sequenceLight4)
+        sequenceLightList.append(sequenceLight5)
+        sequenceLightList.append(sequenceLight5)
+        sequenceLightList.append(sequenceLight6)
+        sequenceLightList.append(sequenceLight7)
+        sequenceLightList.append(sequenceLight8)
+        sequenceLightList.append(sequenceLight9)
+        sequenceLightList.append(sequenceLight10)
+        sequenceLightList.append(sequenceLight11)
+        sequenceLightList.append(sequenceLight12)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,6 +55,10 @@ class ViewController: UIViewController, StateObserver {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func onRedButtonTouchUp(sender: SOXShapedTapButton) {
+        self.gameLogic!.checkButtonPressedWithGameButton(GameButton.Red)
+    }
     
     /// Example use of the game logic against a button press.
     func buttonClicked(sender: UIButton)
@@ -47,6 +82,7 @@ class ViewController: UIViewController, StateObserver {
     func onGameFail()
     {
         /// Handle for if the user fails the game.
+        print("fail")
     }
     
     func onGameRoundComplete()
@@ -64,5 +100,6 @@ class ViewController: UIViewController, StateObserver {
         /// Handle for if user has managed a correct single match within a sequence.
         /// We might not need this one, I just have it for posterity's sake.
         /// Feel free to remove this method if you do not use it.
+        print("correct")
     }
 }
